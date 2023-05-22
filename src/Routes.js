af4130b3ee622a651ee21b4a599a4232b1fc6a43
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-05-22 14:38:22 +0530

    Products Page Frontend

--- src/Routes.js
+++ src/Routes.js
@@ -2,5 +2,6 @@ import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import ActivationPage from "./pages/ActivationPage";
+import ProductsPage from "./pages/ProductsPage";
 
-export { HomePage, LoginPage, SignUpPage, ActivationPage };
+export { HomePage, LoginPage, SignUpPage, ActivationPage, ProductsPage };
