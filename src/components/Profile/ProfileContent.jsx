d5e0e70209b407f0825c392811760fd3858e891d
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-05-25 11:45:28 +0530

    Mobile Responsive Header and SIdebar

--- src/components/Profile/ProfileContent.jsx
+++ src/components/Profile/ProfileContent.jsx
@@ -25,116 +25,101 @@ const ProfileContent = ({ active }) => {
   };
   return (
     <div className="w-full">
-      {" "}
-      {/* Profile Page */}{" "}
+      {/* Profile Page */}
       {active === 1 && (
         <>
-          {" "}
           <div className="flex justify-center w-full">
-            {" "}
             <div className="relative">
-              {" "}
               <img
                 src={`${backend_url}${user?.avatar}`}
                 alt="my profile"
                 className="w-[150px] h-[150px] rounded-full object-cover border-[3px] border-[#3ad132] "
-              />{" "}
+              />
               <div className="w-[30px] h-[30px] bg-[#e3e9ee] rounded-full flex items-center justify-center cursor-pointer absolute bottom-[5px] right-[5px]">
-                {" "}
-                <AiOutlineCamera />{" "}
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
-          <br /> <br />{" "}
+                <AiOutlineCamera />
+              </div>
+            </div>
+          </div>
+          <br />
+          <br />
           <div className="w-full px-5">
-            {" "}
             <form onSubmit={handleSubmit} aria-required={true}>
-              {" "}
-              <div className="w-full flex pb-3">
-                {" "}
-                <div className="w-[50%]">
-                  {" "}
-                  <label className="block pb-2">Full Name</label>{" "}
+              <div className="w-full 800px:flex block pb-3">
+                <div className="w-full 800px:w-[50%]">
+                  <label className="block pb-2">Full Name</label>
                   <input
                     type="text"
-                    className={`${styles.input} !w-[95%] `}
+                    className={`${styles.input} !w-[95%] mb-4 800px:mb-0 `}
                     required
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                  />{" "}
-                </div>{" "}
-                <div className="w-[50%]">
-                  {" "}
-                  <label className="block pb-2">Email Address</label>{" "}
+                  />
+                </div>
+                <div className="w-full 800px:w-[50%]">
+                  <label className="block pb-2">Email Address</label>
                   <input
                     type="email"
-                    className={`${styles.input} !w-[95%] `}
+                    className={`${styles.input} !w-[95%] mb-1 800px:mb-0 `}
                     required
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                  />{" "}
-                </div>{" "}
-              </div>{" "}
-              <div className="w-full flex pb-3">
-                {" "}
-                <div className="w-[50%]">
-                  {" "}
-                  <label className="block pb-2">Phone Number</label>{" "}
+                  />
+                </div>
+              </div>
+              <div className="w-full 800px:flex block pb-3">
+                <div className="w-full 800px:w-[50%]">
+                  <label className="block pb-2">Phone Number</label>
                   <input
                     type="number"
-                    className={`${styles.input} !w-[95%] `}
+                    className={`${styles.input} !w-[95%] mb-1 800px:mb-0 `}
                     required
                     value={phoneNumber}
                     onChange={(e) => setPhoneNumber(e.target.value)}
-                  />{" "}
-                </div>{" "}
-                <div className="w-[50%]">
-                  {" "}
-                  <label className="block pb-2">Zip Code</label>{" "}
+                  />
+                </div>
+                <div className="w-full 800px:w-[50%]">
+                  <label className="block pb-2">Zip Code</label>
                   <input
                     type="number"
-                    className={`${styles.input} !w-[95%] `}
+                    className={`${styles.input} !w-[95%] mb-1 800px:mb-0 `}
                     required
                     value={zipCode}
                     onChange={(e) => setZipCode(e.target.value)}
-                  />{" "}
-                </div>{" "}
-              </div>{" "}
-              <div className="w-full flex pb-3">
-                {" "}
-                <div className="w-[50%]">
-                  {" "}
-                  <label className="block pb-2">Address 1</label>{" "}
+                  />
+                </div>
+              </div>
+              <div className="w-full 800px:flex block pb-3">
+                <div className="w-full 800px:w-[50%]">
+                  <label className="block pb-2">Address 1</label>
                   <input
                     type="text"
-                    className={`${styles.input} !w-[95%] `}
+                    className={`${styles.input} !w-[95%] mb-1 800px:mb-0 `}
                     required
                     value={address1}
                     onChange={(e) => setAddress1(e.target.value)}
-                  />{" "}
-                </div>{" "}
-                <div className="w-[50%]">
-                  {" "}
-                  <label className="block pb-2">Address 2</label>{" "}
+                  />
+                </div>
+                <div className="w-full 800px:w-[50%]">
+                  <label className="block pb-2">Address 2</label>
                   <input
                     type="text"
-                    className={`${styles.input} !w-[95%] `}
+                    className={`${styles.input} !w-[95%] mb-1 800px:mb-0 `}
                     required
                     value={address2}
                     onChange={(e) => setAddress2(e.target.value)}
-                  />{" "}
-                </div>{" "}
-              </div>{" "}
+                  />
+                </div>
+              </div>
               <input
-                className={`w-[250px] h-[40px] border border-[#3a24db] text-center text--[#3a24db] rounded-[3px] mt-8 cursor-pointer`}
+                className={`w-[250px] h-[40px] border border-[#3a24db] text-center text--[#3a24db] rounded-[3px] mt-4 cursor-pointer`}
                 type="submit"
                 value="Update"
                 required
-              />{" "}
-            </form>{" "}
-          </div>{" "}
+              />
+            </form>
+          </div>
         </>
-      )}{" "}
+      )}
       {/* Order page */}
       {active === 2 && (
         <div>
